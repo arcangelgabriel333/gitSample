867eb24d52afeff5060d46dc8175acaf2d79a299
antonio <antonio@gmail.com>
2019-08-13 09:42:31 +0200

    implemented clock

--- gitSample/ViewController.swift
+++ gitSample/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("hello world")
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(handleTimer), userInfo: nil, repeats: true)
+
     }
-
-
+    
+    @objc func handleTimer() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        labelText.text = formatter.string(from: Date())
+    }
 }
 
